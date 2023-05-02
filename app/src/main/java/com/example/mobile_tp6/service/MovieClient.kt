e1ef358761c57d275026b3b8dc519b5aad152b60
francaparroz21 <francaparroz21@gmail.com>
2023-05-01 23:53:58 -0300

    request Interceptor added, app not working

--- app/src/main/java/com/example/mobile_tp6/service/MovieClient.kt
+++ app/src/main/java/com/example/mobile_tp6/service/MovieClient.kt
@@ -1,10 +1,13 @@
 package com.example.mobile_tp6.service
+
 import retrofit2.Call
 import com.example.mobile_tp6.service.model.MovieList
 import retrofit2.http.GET
+import retrofit2.http.Headers
 
 interface MovieClient {
 
-    @GET("/movies/popular?api_key=c14f9e72fded7c99ea38539b590ef335")
+    @Headers("api_key: " + "c14f9e72fded7c99ea38539b590ef335")
+    @GET("/movie/popular")
     fun getData(): Call<MovieList>
 }
--- app/src/main/java/com/example/mobile_tp6/service/MovieRequestGenerator.kt
+++ app/src/main/java/com/example/mobile_tp6/service/MovieRequestGenerator.kt
@@ -1,20 +1,34 @@
 package com.example.mobile_tp6.service
 
+import okhttp3.HttpUrl
+import okhttp3.Interceptor
 import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-import retrofit2.create
 
 object MovieRequestGenerator {
-    private const val API_MOVIES_URL = "https://api.themoviedb.org/3/"
-    private val httpClient = OkHttpClient.Builder()
+    private const val API_MOVIES_URL = "https://api.themoviedb.org/3"
+    private const val API_KEY = "c14f9e72fded7c99ea38539b590ef335"
+
+    private val requestInterceptor = Interceptor { chain ->
+        val url: HttpUrl = chain.request()
+            .url().newBuilder().addQueryParameter("api_key", API_KEY).build()
+
+        val req: Request = chain.request().newBuilder().url(url).build()
+
+        return@Interceptor chain.proceed(req)
+    }
+
+    private val httpClient = OkHttpClient.Builder().addInterceptor(requestInterceptor)
 
     private val builder = Retrofit.Builder()
         .baseUrl(API_MOVIES_URL)
         .addConverterFactory(GsonConverterFactory.create())
 
 
-    fun <S> createService(serviceClass: Class<S>):S{
+    fun <S> createService(serviceClass: Class<S>): S {
         val retrofit = builder.client(httpClient.build()).build()
         return retrofit.create(serviceClass)
     }

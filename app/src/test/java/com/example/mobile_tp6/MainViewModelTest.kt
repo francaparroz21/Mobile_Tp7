373918828a73ca9a0360715c428f748c4fa47ac2
francaparroz21 <francaparroz21@gmail.com>
2023-05-04 19:37:21 -0300

    agregando mocks y fun setup

--- app/src/test/java/com/example/mobile_tp6/MainViewModelTest.kt
+++ app/src/test/java/com/example/mobile_tp6/MainViewModelTest.kt
@@ -1,8 +1,30 @@
 package com.example.mobile_tp6
-
-import org.junit.Rule
-import org.junit.rules.TestRule
+import com.example.mobile_tp6.database.MovieDatabase
+import com.example.mobile_tp6.mvvm.contract.MainContract
+import com.example.mobile_tp6.mvvm.viewmodel.MainViewModel
+import com.example.mobile_tp6.service.model.Movie
+import io.mockk.MockKAnnotations
+import io.mockk.impl.annotations.MockK
+import org.junit.Before
 
 class MainViewModelTest {
+
+    @MockK
+    private lateinit var model: MainContract.Model
+
+    private lateinit var viewModel: MainViewModel
+
+    @MockK
+    private lateinit var movieList: List<Movie>
+
+    @MockK
+    private lateinit var database: MovieDatabase
+
+    @Before
+    fun setup(){
+        MockKAnnotations.init(this, relaxed = true)
+        viewModel = MainViewModel(model)
+    }
+
 
 }
